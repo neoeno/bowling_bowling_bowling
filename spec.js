d5a3723d740cac007390b27c062cadc08077ed65
Kay Lovelace <kay@makersacademy.com>
2017-05-23 15:07:59 +0100

    Extract TestFrameworkReporter from TestFramework

--- spec.js
+++ spec.js
@@ -3,38 +3,12 @@ const chalk = require('chalk')
 const TEST_FAILED = "fhewaighewuigawueighuiaweghewui"
 const TEST_PASSED = "gewahgaewgeawjgieawjgieawogaewi"
 
-function TestFrameworkFactory() {
-  const tests = []
-
-  function it(message, testFn) {
-    tests.push({
-      message: message,
-      testFn: testFn
-    })
-  }
-
-  function runTests() {
-    const testSuiteStatus = tests.reduce(function(statusSoFar, test) {
-      if (statusSoFar === TEST_PASSED) {
-        return runSingleTest(test)
-      } else {
-        runSingleTest(test)
-        return TEST_FAILED
-      }
-    }, TEST_PASSED)
-
-    reportSuiteStatus(testSuiteStatus)
-  }
-
-  function runSingleTest(test) {
-    console.log(chalk.cyan(test.message))
-    try {
-      test.testFn()
+function TestFrameworkReporterFactory() {
+  function reportSingleTestResult(testResult) {
+    if (testResult === TEST_PASSED) {
       console.log(chalk.green("Passed!"))
-      return TEST_PASSED
-    } catch (e) {
-      console.log(chalk.red("Test failed! %s"), e)
-      return TEST_FAILED
+    } else {
+      console.log(chalk.red("Test failed!"))
     }
   }
 
@@ -46,9 +20,52 @@ function TestFrameworkFactory() {
     }
   }
 
+  return {
+    reportSingleTestResult: reportSingleTestResult,
+    reportSuiteStatus: reportSuiteStatus
+  }
+}
+
+function TestFrameworkFactory() {
+  const tests = []
+  const reporter = TestFrameworkReporterFactory()
+
+  function it(message, testFn) {
+    tests.push({
+      message: message,
+      testFn: testFn
+    })
+  }
+
+  function runTests() {
+    const testSuiteStatus = tests.reduce(function(statusSoFar, test) {
+      if (statusSoFar === TEST_PASSED) {
+        const testResult = runSingleTest(test)
+        reporter.reportSingleTestResult(testResult)
+        return testResult
+      } else {
+        const testResult = runSingleTest(test)
+        reporter.reportSingleTestResult(testResult)
+        return TEST_FAILED
+      }
+    }, TEST_PASSED)
+
+    reporter.reportSuiteStatus(testSuiteStatus)
+  }
+
+  function runSingleTest(test) {
+    console.log(chalk.cyan(test.message))
+    try {
+      test.testFn()
+      return TEST_PASSED
+    } catch (e) {
+      return TEST_FAILED
+    }
+  }
+
   function assertEqual(a, b) {
     if (a != b) {
-      throw a + " is not equal to " + b
+      throw "Nothing of any importance"
     }
   }
 
