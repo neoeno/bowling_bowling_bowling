f9864553909a695899a686b3cbbd8d468c3b3942
Kay Lovelace <kay@makersacademy.com>
2017-05-23 15:03:11 +0100

    Encapsulate test framework

--- spec.js
+++ spec.js
@@ -1,46 +1,73 @@
 const chalk = require('chalk')
 
-const tests = []
+const TEST_FAILED = "fhewaighewuigawueighuiaweghewui"
+const TEST_PASSED = "gewahgaewgeawjgieawjgieawogaewi"
 
-function it(message, testFn) {
-  tests.push({
-    message: message,
-    testFn: testFn
-  })
-}
+function TestFrameworkFactory() {
+  const tests = []
 
-function runTests() {
-  var haveMyTestsFailed = false
-  tests.forEach(function(test) {
+  function it(message, testFn) {
+    tests.push({
+      message: message,
+      testFn: testFn
+    })
+  }
+
+  function runTests() {
+    const testSuiteStatus = tests.reduce(function(statusSoFar, test) {
+      if (statusSoFar === TEST_PASSED) {
+        return runSingleTest(test)
+      } else {
+        runSingleTest(test)
+        return TEST_FAILED
+      }
+    }, TEST_PASSED)
+
+    reportSuiteStatus(testSuiteStatus)
+  }
+
+  function runSingleTest(test) {
     console.log(chalk.cyan(test.message))
     try {
       test.testFn()
       console.log(chalk.green("Passed!"))
+      return TEST_PASSED
     } catch (e) {
-      haveMyTestsFailed = true
       console.log(chalk.red("Test failed! %s"), e)
+      return TEST_FAILED
     }
-  })
-  if (haveMyTestsFailed) {
-    console.log("At least one of your tests failed")
-  } else {
-    console.log(chalk.green("All your tests passed!"))
+  }
+
+  function reportSuiteStatus(status) {
+    if (status === TEST_PASSED) {
+      console.log(chalk.green("All your tests passed!"))
+    } else {
+      console.log(chalk.red("At least one of your tests failed"))
+    }
+  }
+
+  function assertEqual(a, b) {
+    if (a != b) {
+      throw a + " is not equal to " + b
+    }
+  }
+
+  return {
+    it: it,
+    runTests: runTests,
+    assertEqual: assertEqual
   }
 }
 
-function assertEqual(a, b) {
-  if (a != b) {
-    throw a + " is not equal to " + b
-  }
-}
+const t = TestFrameworkFactory()
 
-it("asserts 1 == 1", function() {
-  assertEqual(1, 1)
+t.it("asserts 1 == 1", function() {
+  t.assertEqual(1, 1)
 })
 
-it("forbids 1 == 2", function() {
-  assertEqual(1, 2)
+t.it("forbids 1 == 2", function() {
+  t.assertEqual(1, 2)
 })
 
-runTests()
+t.runTests()
 
